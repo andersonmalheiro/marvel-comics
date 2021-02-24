247cda0fa039873d94a67019cafd9cacafef70ca
Anderson Malhero de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-02-24 16:42:49 -0300

    [main] - Adding utils module

---
+++ src/utils/useAPIAuth.ts
@@ -0,0 +1,12 @@
+import md5 from 'md5';
+import { config } from 'config';
+
+export const useAPIAuth = () => {
+  const ts = Date.now().toString();
+  const hash = md5(
+    ts +
+      process.env.NEXT_PUBLIC_API_SECRET_KEY +
+      process.env.NEXT_PUBLIC_API_PUBLIC_KEY
+  );
+  return [ts, hash, process.env.NEXT_PUBLIC_API_PUBLIC_KEY];
+};
